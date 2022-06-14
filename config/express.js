0a72ed96f89e14abccbcd63082358c4c4018fae4
sbabukov <s.babukov@gmail.com>
2022-06-14 17:57:01 +0300

    extract express config

---
+++ config/express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {engine}  = require('express-handlebars');
+
+
+function setupExpress(app) {
+    app.engine('hbs', engine({
+        extname: 'hbs'
+    }));
+    // сетъпваме хендълбарса да работи с файлове .hbs
+    app.set('view engine', 'hbs')
+    // сетваме, че вю енджина е хбс
+    
+    app.use(express.static('public'))
+    // сетъпваме папка public
+};
+// функция която очаква да й се подаде арр и да конфигурира
+
+module.exports = setupExpress;
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const config = require('./config/config');
-const {engine}  = require('express-handlebars');
+const expressConfig = require('./config/express');
 
 const app = express();
+expressConfig(app);
+// app се праща за конфигурация към expressConfig
 
-app.engine('hbs', engine({
-    extname: 'hbs'
-}));
-// сетъпваме хендълбарса да работи с файлове .hbs
-app.set('view engine', 'hbs')
-// сетваме, че вю енджина е хбс
 
-app.use(express.static('public'))
-// сетъпваме папка public
+// app.engine('hbs', engine({
+//     extname: 'hbs'
+// }));
+// // сетъпваме хендълбарса да работи с файлове .hbs
+// app.set('view engine', 'hbs')
+// // сетваме, че вю енджина е хбс
+// app.use(express.static('public'))
+// // сетъпваме папка public
+// горното цялото се изнася във файла express.js
 
 
 app.get('/', (req, res) => {
