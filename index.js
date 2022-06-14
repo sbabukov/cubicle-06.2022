48d70ae947b6575e043b27e4181985ab48df4301
sbabukov <s.babukov@gmail.com>
2022-06-14 17:42:59 +0300

    add view engine and home page

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const config = require('./config/config');
-
+const {engine}  = require('express-handlebars');
 
 const app = express();
+
+app.engine('hbs', engine({
+    extname: 'hbs'
+}));
+// сетъпваме хендълбарса да работи с файлове .hbs
+app.set('view engine', 'hbs')
+// сетваме, че вю енджина е хбс
+
+app.use(express.static('public'))
+// сетъпваме папка public
+
+
+app.get('/', (req, res) => {
+    // console.log('It`s working');
+
+    res.render('home', {layout: false})
+    // това {layout: false} не дава на хендълбарс да си търси папка layouts
+});
 
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
