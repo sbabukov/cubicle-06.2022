0c17473b5e69663a8b7c91b0f5dc4b6f4a1b88b6
sbabukov <s.babukov@gmail.com>
2022-06-14 16:59:27 +0300

    initial architecture

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const config = require('./config/config');
+
+
+const app = express();
+
+
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
+// стартира се на съответния порт
