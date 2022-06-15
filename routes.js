2f103db4e38b3f188bc2ed18ee61f263d4d2f126
sbabukov <s.babukov@gmail.com>
2022-06-15 12:48:04 +0300

    add about controller

--- routes.js
+++ routes.js
@@ -2,11 +2,30 @@ const {Router} = require('express');
 const router = Router();
 // създаваме си инстанция на раутера
 
-router.get('/', (req, res) => {
-    // console.log('It`s working');
+const productController = require('./controllers/productController');
+// импортваме контролерите
 
-    res.render('home', {layout: false})
-    // това {layout: false} не дава на хендълбарс да си търси папка layouts
-});
+const aboutController = require('./controllers/aboutController');
+
+router.use('/', productController);
+// за всички заявки, които са на / нека productController се погрижи
+// тук се използват раутерите от productController
+router.use('/about', aboutController);
+
+
+// router.get('/', productController.index);
+// router.get('/create', productController.create);
+// // регистрираме контролерите, index и create са методите от productController
 
 module.exports = router;
+
+
+
+
+
+// router.get('/', (req, res) => {
+//     // console.log('It`s working');
+
+//     res.render('home', {layout: false})
+//     // това {layout: false} не дава на хендълбарс да си търси папка layouts
+// });
